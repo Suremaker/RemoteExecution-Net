ec460241bd9557c6a52ce0f9c01670d2ed4cf86e
Suremaker <wojciech.kotlarski@gmail.com>
2013-08-10 22:19:02 +0100

    updated assembly version

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -1,3 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCopyright("Copyright © Wojciech Kotlarski 2013")][assembly: AssemblyVersion("1.0.7.0")][assembly: AssemblyFileVersion("1.0.7.0")]
+[assembly: AssemblyCopyright("Copyright © Wojciech Kotlarski 2013")]
+[assembly: AssemblyVersion("1.0.8.0")]
+[assembly: AssemblyFileVersion("1.0.8.0")]
