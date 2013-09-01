91c0c2984432f742c641b69123b3bff5f764e941
Suremaker <wojciech.kotlarski@gmail.com>
2013-09-02 00:15:30 +0100

    added documentation

--- RemoteExecution/Configurator.cs
+++ RemoteExecution/Configurator.cs
@@ -2,8 +2,15 @@
 
 namespace RemoteExecution
 {
+	/// <summary>
+	/// Remote execution framework configurator.
+	/// </summary>
 	public static class Configurator
 	{
+		/// <summary>
+		/// Configures remote execution framework.
+		/// Registers all supported transport layer providers.
+		/// </summary>
 		public static void Configure()
 		{
 			TransportLayerResolver.Register(new LidgrenProvider());
